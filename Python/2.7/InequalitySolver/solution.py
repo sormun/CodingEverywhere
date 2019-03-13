cf3a328035ea026be739f9a4be1ffdc2caaa107b
sormun <daniela.munteanu@gmail.com>
2019-03-13 19:58:57 -0400

    lesson 2 completed

--- Python/2.7/InequalitySolver/solution.py
+++ Python/2.7/InequalitySolver/solution.py
@@ -1,11 +1,14 @@
 b = 20
 t = 254
 f = 26
-
+print
+print "b is the height of the first floor.","The value is ",b," ","feet","."
+print "t is a height smaller than the building's height.","The value is ",t," ","feet","."
+print "f is the number of remaining floors.","The value is ",f," ","","."
 print "x is the average height of the remaining floors"
 
 print "b+f*x > t"
 
 print "x > (t-b)/f"
 
-print "x > ",(t-b)/f
+print "x > ",(t-b)/f," ","feet"
