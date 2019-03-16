53977c00f846f4402138c446b3b65d831ef49aea
sormun <daniela.munteanu@gmail.com>
2019-03-16 14:16:35 -0400

    lesson 5 completed

--- Python/2.7/InequalitySolver/solution.py
+++ Python/2.7/InequalitySolver/solution.py
@@ -1,12 +1,16 @@
 def print_variable(name,description,value,units):
   print name," is ",description,".","The value is ",value," ",units,"."
+def display_problem_from_file(filename):
+  print
+  with open(filename,'r') as file: problem=file.read()
+  print problem
+  print
+
 b = 20
 t = 254
 f = 26
-print
-with open('problem.txt','r') as file: problem=file.read()
-print problem
-print
+
+display_problem_from_file('problem.txt')
 print_variable("b","the height of the first floor",20,"feet")
 print_variable("t","a height smaller than the building's height",254,"feet")
 print_variable("f","the number of the remaining floors",26,"")
