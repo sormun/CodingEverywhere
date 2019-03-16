1fb10adf606952d5675932f2434e863237b5e6aa
sormun <daniela.munteanu@gmail.com>
2019-03-16 16:28:12 -0400

    lesson 9 completed

--- Python/2.7/InequalitySolver/solution.py
+++ Python/2.7/InequalitySolver/solution.py
@@ -5,6 +5,12 @@ def display_problem_from_file(filename):
   with open(filename,'r') as file: problem=file.read()
   print problem
   print
+def present_problem(filename,variables,formula):
+  display_problem_from_file(filename)
+  for variable in variables:print_variable(*variable)
+  print formula
+  print
+  
 variables=(
 ("b","the height of the first floor",20,"feet")
 ,("t","a height smaller than the building's height",254,"feet")
@@ -12,15 +18,15 @@ variables=(
 ,("x","the average height of the remaining floors","unknown","feet")
 )
 
-display_problem_from_file('problem.txt')
-for variable in variables:print_variable(*variable)
+formula= "b+f*x > t"
+present_problem('problem.txt',variables,formula)
 
 b = variables[0][2]
 t = variables[1][2]
 f = variables[2][2]
 
 
-print "b+f*x > t"
+
 
 print "x > (t-b)/f"
 
