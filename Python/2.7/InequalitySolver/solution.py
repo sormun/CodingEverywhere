b9346bca6aa0b5f16a5caf3b1acbc34b1910629f
sormun <daniela.munteanu@gmail.com>
2019-03-17 19:09:04 -0400

    lesson 29 completed

--- Python/2.7/InequalitySolver/solution.py
+++ Python/2.7/InequalitySolver/solution.py
@@ -38,6 +38,26 @@ if len(lhs_tokens)==0:
   lhs_tokens.append("0");
 
 
+for i in range(len(rhs_tokens)):
+  token=rhs_tokens.pop(0)
+  if token in ["0","-0","+0"]:continue
+  if "x" not in token:
+    if token.startswith("+") or token.startswith("-"):
+      rhs_tokens.append(token)
+    else:
+      rhs_tokens.append("+"+token)
+  else:
+    if token.startswith("-"):neg_token="+"+token[1:]
+    elif token.startswith("+"):neg_token="-"+token[1:]
+    else: neg_token="-"+token
+    lhs_tokens.append(neg_token)
+    if len(rhs_tokens)==0:
+      rhs_tokens.append("0");
+    present_formula(lhs_tokens,f_sign,rhs_tokens)
+if len(rhs_tokens)==0:
+  rhs_tokens.append("0");
+
+
 print
 
 print "x ",f_sign," (t-b)/f"
