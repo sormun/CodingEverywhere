c04ab224c4134aec78434c6cfb64e503fde56ec1
sormun <daniela.munteanu@gmail.com>
2019-05-12 19:07:19 -0400

    lesson 36 completed

--- Python/2.7/InequalitySolver/solution.py
+++ Python/2.7/InequalitySolver/solution.py
@@ -1,4 +1,5 @@
 from codingeverywhere_library import present_problem,read_problem,parse_formula,present_formula,solve_phase_1
+from codingeverywhere_library import printable_token
 import sys
 import re
 
@@ -17,6 +18,21 @@ canonical_formula=formula.replace(" ","")
 lhs_tokens,f_sign,rhs_tokens=parse_formula(canonical_formula)
 
 lhs_tokens,f_sign,rhs_tokens=solve_phase_1(lhs_tokens,f_sign,rhs_tokens,"x")
+
+lhs="("
+for i in range(len(lhs_tokens)):
+  lhs+=printable_token(i,lhs_tokens[i])
+lhs+=")"
+
+rhs="("
+for i in range(len(rhs_tokens)):
+  rhs+=printable_token(i,rhs_tokens[i])
+rhs+=")"
+
+print 
+print "Phase II:"
+print lhs,f_sign,rhs
+
 
 print
 if sys.argv[1] in ["analysis.json","analysis_002.json","analysis_003.json","analysis_004.json","analysis_006.json"]:
